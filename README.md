fe5c6f7e92dec45a1ea674dda0dfae5f3540985e
Andrew-Day-Coder <andrew.day21@fryeburgacademy.org>
2020-03-04 12:41:15 +0000

    Added many source code doc comments as well as added a bit to readme.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HOG2
+
+This is the source code for Fryeburg Academy's Heart of Gold 2 robot.
--- include/Lift.h
+++ include/Lift.h
@@ -11,7 +11,7 @@ enum class TARGET
 };
 class Lift
 {
-  private:
+  private;
     vex::motor* leftMotor;
     vex::motor* rightMotor;
     bool (*isAtBottom)(void);
@@ -29,23 +29,93 @@ class Lift
     const double liftTolerance = 0.5;
 
 
+    /**
+     * Sets the power of the lift, used for when the driver
+     * is attempting to use the lift, a lift to fast, will
+     * be difficult to control, but too slow, and it wastes time.
+     *
+     * @param units - the value, in percent, that the motors
+     * will be powered with when lifting, and lowering.
+     */
 
     void setPower(int);
+    /**
+     * Sets a rotation point, in degrees, that the motor
+     * will attempt to go to. 
+     *
+     * @param value - the value, in degrees, that the motors
+     * will attempt to go to.
+     */
     void setEncoderTargetPoint(double);
+    /**
+     * Do not spin the motors, at all, don't hold, just let them `vex::brakeType::coast`    
+     */
     void release();
   public:
+    /**
+     * Creates a new lift
+     * 
+     * @param left - the left motor,
+     * @param rigth - the right motor 
+     */
     Lift(vex::motor*, vex::motor*);
+
+    /**
+     * Associates a function that can tell when the lift
+     * when it has reached the bottom.
+     *
+     * @param bottomLimit - a function pointer to a function that can
+     * tell the code when it is at the bottom of the lift.
+     * @deprecated
+     */
     void attachIsAtBottomFunction(bool (*bottomLimit)(void));
+    /**
+     * Function to be called on every tick of the lift for the lift
+     */
     void update();
+    /**
+     * A way to request if the lift has a way to tell if it
+     * can tell if it is at the bottom
+     *
+     * @returns true if lift can determine the bottom, else false
+     * @deprecated
+     */
     bool hasBottomFunction() { return isAtBottom != nullptr; }
+    /**
+     * Zeroes the motor encoders
+     */
     void zero();
+    /**
+     * Makes the lift attempt to find it's bottom, must
+     * have a "bottoming" function associated with it
+     */
     void findBottom();
+    /**
+     * Holds the lift where it is
+     */
     void hold();
+    /**
+     * Makes the lift go up with whatever power is passed
+     * to the setPower funtion
+     */
     void up();
+    /**
+     * Makes the lift go down with whatever power is passed
+     * to the setPower funtion
+     */
     void down();
 
+    /**
+     * Makes the lift raise, by setting the encoder target.
+     */
     void smoothUp();
+    /**
+     * Makes the lift lower by setting the encoder target
+     */
     void smoothDown();
+    /**
+     * resets the encoder target location, useful for canceling actions
+     */
     void resetTargetLocation();
     
     void setTargetLocation(TARGET);
